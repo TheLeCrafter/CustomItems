81b8946b270c864a6760375fbd768b423b20dd2b
TheLeCrafter <47987588+TheLeCrafter@users.noreply.github.com>
2021-04-24 12:19:00 +0200

    Initial commit

---
+++ src/main/java/dev/thelecrafter/dimensionz/items/ItemsPlugin.java
@@ -0,0 +1,12 @@
+package dev.thelecrafter.dimensionz.items;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class ItemsPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+}
